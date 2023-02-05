28be81d3c44810303b7d0690fa187179de6b1041
unknown <awaishk640@gmail.com>
2023-02-05 23:53:50 +0530

    Added build

--- server.js
+++ server.js
@@ -655,7 +655,7 @@ io.on("connection", (socket) => {
 })
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "static/index.html"))
+    res.sendFile(path.join(__dirname, "build/index.html"))
 })
 
 app.listen(4000, () => {
